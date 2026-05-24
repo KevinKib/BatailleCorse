664a84de2194fb3b4f3464fcda04391c9856f51b
Kévin Kib <sekanor21@gmail.com>
2026-05-25 00:20:40 +0200

    style: fix import ordering in CreateEventData

--- backend/src/main/java/org/kevinkib/bataillecorse/websocket/presentation/v1/dto/event/CreateEventData.java
+++ backend/src/main/java/org/kevinkib/bataillecorse/websocket/presentation/v1/dto/event/CreateEventData.java
@@ -1,8 +1,8 @@
 package org.kevinkib.bataillecorse.websocket.presentation.v1.dto.event;
 
-import org.kevinkib.bataillecorse.websocket.presentation.v1.dto.BatailleCorseIdDto;
-
 import java.util.Map;
+
+import org.kevinkib.bataillecorse.websocket.presentation.v1.dto.BatailleCorseIdDto;
 
 public record CreateEventData(BatailleCorseIdDto game, Map<Integer, String> tokens) implements EventData {
 }
